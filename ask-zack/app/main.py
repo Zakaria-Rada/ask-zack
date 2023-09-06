f9721e75619b3a4b6c0445b86c5723f72e6cc717
Zakaria rada <zakaria.rada@tridge.com>
2023-09-06 11:37:23 +0900

    [Feature] enhnace docker file

--- ask-zack/app/main.py
+++ ask-zack/app/main.py
@@ -18,13 +18,18 @@ async def get_form():
 
 @app.post("/", response_class=HTMLResponse)
 async def read_root(user_input: str = Form(...)):
-    os.chdir('/llama.cpp')
+    # Make sure this path to the executable is correct
+    executable_path = "/llama.cpp/main"
+
     command = [
-        "./main", "-t", "8", "-ngl", "1", "-m", "llama-2-13b-chat.ggmlv3.q4_0.bin",
-        "--color", "-c", "2048", "--temp", "0.7", "--repeat_penalty", "1.1",
-        "-n", "-1", "-p", f"[PROMPT] {user_input} [/PROMPT]"
+        executable_path, "-t", "8", "-ngl", "1",
+        "-m", "llama-2-13b-chat.ggmlv3.q4_0.bin", "--color", "-c", "2048",
+        "--temp", "0.7", "--repeat_penalty", "1.1", "-n", "-1",
+        "-p", f"[PROMPT] {user_input} [/PROMPT]"
     ]
+
     result = subprocess.run(command, capture_output=True, text=True)
     output = result.stdout
+
     html_content = f"<html><body><pre>{output}</pre></body></html>"
     return html_content
