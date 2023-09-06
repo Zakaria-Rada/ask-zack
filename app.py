310e2632b9cd1ae3ba8836693c59c201136e09c2
Zakaria rada <zakaria.rada@tridge.com>
2023-09-06 14:06:12 +0900

    [Feature] add flask code

--- app.py
+++ app.py
@@ -1,33 +1,30 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, jsonify, request, render_template
 import subprocess
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 
 @app.route('/ask', methods=['POST'])
 def ask():
-    prompt = request.json['prompt']
-    docker_command = f"docker run -e PROMPT=\"{prompt}\" zack-llama2:0.0.1"
+    question = request.form['question']
 
     try:
-        result = subprocess.run(
-            docker_command,
-            shell=True,
-            check=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True,
+        completed_process = subprocess.run(
+            ["docker", "run", "-e", f'PROMPT={question}', "zack-llama2:0.0.1"],
+            capture_output=True, text=True
         )
-        output = result.stdout
-    except subprocess.CalledProcessError as e:
-        output = f"An error occurred: {str(e)}"
 
-    return jsonify({'response': output})
+        output = completed_process.stdout
+        return jsonify({"response": output})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5050)
