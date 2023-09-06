2477068ea04e8a65d351103f4d81f753d76e733b
Zakaria rada <zakaria.rada@tridge.com>
2023-09-06 14:43:03 +0900

    [Feature] add test code

--- app.py
+++ app.py
@@ -12,21 +12,19 @@ def run_docker_command(prompt):
             capture_output=True,
             text=True
         )
-
         output = completed_process.stdout
         return {"response": output}
-
     except Exception as e:
         return {"error": str(e)}
 
 
 @app.route('/run', methods=['POST'])
 def run_command():
-    data = request.get_json()
-    prompt = data.get('prompt', '')
+    data = request.json
+    prompt = data.get("prompt")
     result = run_docker_command(prompt)
     return jsonify(result)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5050)
