d902f0cc8bd295228ce3e9740f9c6fe1c44fb36f
Zakaria rada <zakaria.rada@tridge.com>
2023-09-07 14:23:31 +0900

    [Feature] add test code

--- zackgpt/app.py
+++ zackgpt/app.py
@@ -1,29 +1,32 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 import subprocess
 import json
 
-app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-
-@app.route('/run', methods=["POST"])
-def run_docker_command():
+def run_docker_command(prompt):
     try:
-        prompt = request.json.get('prompt')
         completed_process = subprocess.run(
             ["docker", "run", "-e", f'PROMPT={prompt}', "zack-llama2:0.0.1"],
             capture_output=True,
             text=True
         )
         output = completed_process.stdout
-        return jsonify({"response": output})
+        return {"response": output}
     except Exception as e:
-        return jsonify({"error": str(e)})
+        return {"error": str(e)}
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=6060, debug=True)
+app = Flask(__name__)
+
+
+@app.route('/run', methods=['POST'])
+def run():
+    if not request.is_json:
+        return jsonify({"error": "Expected JSON payload"}), 415
+
+    prompt = request.json.get('prompt', None)
+    result = run_docker_command(prompt)
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=6060)
