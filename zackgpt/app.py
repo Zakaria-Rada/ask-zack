36a61ebd237dad5a4ca61c476cf4da437f0d321b
Zakaria rada <zakaria.rada@tridge.com>
2023-09-07 14:05:39 +0900

    [Feature] add test code

--- zackgpt/app.py
+++ zackgpt/app.py
@@ -1,35 +1,29 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import subprocess
 import json
 
 app = Flask(__name__)
 
 
-def run_docker_command(prompt):
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/run', methods=["POST"])
+def run_docker_command():
     try:
+        prompt = request.json.get('prompt')
         completed_process = subprocess.run(
             ["docker", "run", "-e", f'PROMPT={prompt}', "zack-llama2:0.0.1"],
             capture_output=True,
             text=True
         )
         output = completed_process.stdout
-        return {"response": output}
-
+        return jsonify({"response": output})
     except Exception as e:
-        return {"error": str(e)}
+        return jsonify({"error": str(e)})
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/run', methods=['POST'])
-def run():
-    prompt = request.json['prompt']
-    result = run_docker_command(prompt)
-    return jsonify(result)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6060)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=6060, debug=True)
