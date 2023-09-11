1e53d225ccfe3f1a6f8bba6f6efd52b46763c76b
Zakaria rada <zakaria.rada@tridge.com>
2023-09-11 17:48:25 +0900

    [feature] model usage

--- zackgpt/prompt.py
+++ zackgpt/prompt.py
@@ -1,20 +1,17 @@
 import os
 import warnings
 import torch
-import gc  # Import garbage collector
+import gc
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 # Suppress all warnings
 warnings.simplefilter("ignore")
 
-# Clear CUDA memory
 torch.cuda.empty_cache()
 
-# Set the GPU device
-GPU_DEVICE = 1  # 0 or 1 depending on which GPU you want to use
+GPU_DEVICE = 1 
 
-# Set the absolute path to the saved model
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Get the current directory's absolute path
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_SAVE_PATH = os.path.join(BASE_DIR, "results", "llama-2-7b-miniguanaco")
 
 # Load saved model and tokenizer from the local directory only once
@@ -22,7 +19,7 @@ MODEL = AutoModelForCausalLM.from_pretrained(MODEL_SAVE_PATH)
 TOKENIZER = AutoTokenizer.from_pretrained(MODEL_SAVE_PATH)
 
 # Move the model to the specified GPU
-MODEL = MODEL.to(f'cuda:{GPU_DEVICE}')
+MODEL = MODEL.to(f'cuda:{GPU_DEVICE}')  
 
 # Create a text generation pipeline only once
 GENERATOR = pipeline("text-generation", model=MODEL, tokenizer=TOKENIZER, device=GPU_DEVICE)
@@ -34,7 +31,6 @@ def generate_text(prompt):
     output = GENERATOR(prompt, max_length=200, num_return_sequences=1)
     generated_text = output[0]['generated_text']
 
-    # Remove the initial user's prompt from the generated text
     generated_text = generated_text[len(prompt):]
 
     return generated_text
@@ -43,7 +39,7 @@ def generate_text(prompt):
 if __name__ == "__main__":
     print("Chatbot is ready. Type 'exit' to end the conversation.\n")
     while True:
-
+        # User's message
         user_prompt = input("You: ")
 
         if user_prompt.strip().lower() == "exit":
@@ -53,6 +49,5 @@ if __name__ == "__main__":
         response = generate_text(user_prompt)
         print("ZackGPT:", response)
 
-        # Clear CUDA cache and run garbage collector for good measure
         torch.cuda.empty_cache()
         gc.collect()
